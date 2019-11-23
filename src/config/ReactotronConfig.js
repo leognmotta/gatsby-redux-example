8281977ef6d8f342d6ba697139aa9c6c687ae676
Leonardo Motta <mail@leomotta.me>
2019-11-22 19:13:22 -0500

    fix(reactotronconfig.js): get ReactotronConfig to work
    ReactotronConfig was not working beucase reactotron-react-js was trying to access window during ssr
    lifecycle of GatsbyJS, my solution was to make my own config protecting the window call
    
    #1

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,8 +1,47 @@
-import Reactotron from 'reactotron-react-js'
+/* eslint-disable no-restricted-globals */
+import { createClient } from 'reactotron-core-client'
 import { reactotronRedux } from 'reactotron-redux'
 import reactotronSaga from 'reactotron-redux-saga'
 
+function getNavigatorProperty(name) {
+  if (!name) return undefined
+  if (typeof window === 'undefined') return undefined
+  if (!window.navigator && typeof window.navigator !== 'object')
+    return undefined
+  return window.navigator[name]
+}
+
 if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
+  const REACTOTRON_ASYNC_CLIENT_ID = '@REACTOTRON/clientId'
+
+  const client = {
+    createSocket: path => new WebSocket(path), // eslint-disable-line
+    host: 'localhost',
+    port: 9090,
+    name: 'React JS App',
+    client: {
+      reactotronLibraryName: 'reactotron-react-js',
+      reactotronLibraryVersion: 'REACTOTRON_REACT_JS_VERSION',
+      platform: 'browser',
+      platformVersion: getNavigatorProperty('platform'),
+      userAgent: getNavigatorProperty('userAgent'),
+      screenWidth: (screen && screen.width) || undefined,
+      screenHeight: (screen && screen.height) || undefined,
+      screenScale: (window && window.devicePixelRatio) || 1,
+      windowWidth: (window && window.innerWidth) || undefined,
+      windowHeight: (window && window.innerHeight) || undefined,
+    },
+    getClientId: () => {
+      return Promise.resolve(localStorage.getItem(REACTOTRON_ASYNC_CLIENT_ID))
+    },
+    setClientId: clientId => {
+      localStorage.setItem(REACTOTRON_ASYNC_CLIENT_ID, clientId)
+      return Promise.resolve()
+    },
+  }
+
+  const Reactotron = createClient(client)
+
   const tron = Reactotron.configure()
     .use(reactotronRedux())
     .use(reactotronSaga())
